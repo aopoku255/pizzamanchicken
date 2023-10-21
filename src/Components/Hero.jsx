ff4298177c08434dd905dc261458d98928cfbc98
Daniel <danielboadu594@gmail.com>
2023-10-21 14:05:38 +0000

    Order Now

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -1,14 +1,12 @@
-
 import { useState, useEffect } from "react";
 import { sliderData } from "../Data/Hero";
 import "./styles.css";
 import Navbar2 from "./Navbar2";
 import { RiWhatsappFill } from "react-icons/ri";
-import now from '../assets/now.jpg'
-
+import now from "../assets/now.jpg";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
-  
   const [currentSlide, setCurrentSlide] = useState(0);
   const slideLength = sliderData.length;
 
@@ -20,8 +18,6 @@ const Hero = () => {
     setCurrentSlide(currentSlide === slideLength - 1 ? 0 : currentSlide + 1);
     console.log("next");
   };
-
-
 
   function auto() {
     slideInterval = setInterval(nextSlide, intervalTime);
@@ -39,58 +35,66 @@ const Hero = () => {
   }, [currentSlide]);
 
   return (
-   <div className="relative">
-       <Navbar2/>
-    <div className="slider">
-      {sliderData.map((slide, index) => {
-        return (
-          <div
-            className={index === currentSlide ? "slide current" : "slide"}
-            key={index}
-          >
-            {index === currentSlide && (
-        
-              <div className="h-screen w-screen" style={{
-                backgroundImage:`url(${slide.image})`,
-                backgroundRepeat:'no-repeat',
-                backgroundPosition: 'center',
-                backgroundSize:'cover'
-              }}>
-              <div className="absolute top-0 left-0 w-full h-screen bg-black opacity-40"></div>
-              <div className="content">
-                <h2 className="text-xl font-bold">{slide.heading}</h2>
-                <p>{slide.desc} {index === 0 ? <b>0302753430</b> : <></>}</p>
-                <hr />
-              <div className='md:flex flex-row  items-center md:gap-x-4 md:gap-y-0 gap-y-4'>
-                <button className='relative text-white bg-primary px-10 py-3 hover:opacity-70 transition-all duration-400'>
-                  <a href="https://wa.me/+233553871228?text=Hello! Pizzaman Chickenman" target="_blank" class="flex gap-2 items-center">
-                  <RiWhatsappFill />
-                  <span>{slide.button}</span>
-                  </a>
-                  </button>
-                  <button className='relative text-white md:mt-0 mt-4 bg-primary px-10 py-3 hover:opacity-70 transition-all duration-400'>
-                  <a href="https://wa.me/+233553871228?text=Hello! Pizzaman Chickenman" target="_blank" class="flex gap-2 items-center">
-                  {/* <AiOutlineShoppingCart /> */}
-                  <div className="w-5 ">
-                    <img src={now} className="rounded-full w-full"/>
+    <div className="relative">
+      <Navbar2 />
+      <div className="slider">
+        {sliderData.map((slide, index) => {
+          return (
+            <div
+              className={index === currentSlide ? "slide current" : "slide"}
+              key={index}
+            >
+              {index === currentSlide && (
+                <div
+                  className="h-screen w-screen"
+                  style={{
+                    backgroundImage: `url(${slide.image})`,
+                    backgroundRepeat: "no-repeat",
+                    backgroundPosition: "center",
+                    backgroundSize: "cover",
+                  }}
+                >
+                  <div className="absolute top-0 left-0 w-full h-screen bg-black opacity-40"></div>
+                  <div className="content">
+                    <h2 className="text-xl font-bold">{slide.heading}</h2>
+                    <p>
+                      {slide.desc} {index === 0 ? <b>0302753430</b> : <></>}
+                    </p>
+                    <hr />
+                    <div className="md:flex flex-row  items-center md:gap-x-4 md:gap-y-0 gap-y-4">
+                      <button className="relative text-white bg-primary px-10 py-3 hover:opacity-70 transition-all duration-400">
+                        <a
+                          href="https://wa.me/+233553871228?text=Hello! Pizzaman Chickenman"
+                          target="_blank"
+                          class="flex gap-2 items-center"
+                        >
+                          <RiWhatsappFill />
+                          <span>{slide.button}</span>
+                        </a>
+                      </button>
+                      <button className="relative text-white md:mt-0 mt-4 bg-primary px-10 py-3 hover:opacity-70 transition-all duration-400">
+                        <Link
+                          to="menu"
+                          target="_blank"
+                          class="flex gap-2 items-center"
+                        >
+                          {/* <AiOutlineShoppingCart /> */}
+                          <div className="w-5 ">
+                            <img src={now} className="rounded-full w-full" />
+                          </div>
+                          <span>{slide.order}</span>
+                        </Link>
+                      </button>
+                    </div>
                   </div>
-                  <span>{slide.order}</span>
-                  </a>
-                  </button>
-              </div>
-              </div>
+                </div>
+              )}
             </div>
-              
-
-            )}
-          </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
-   </div>
   );
 };
 
 export default Hero;
-
-
